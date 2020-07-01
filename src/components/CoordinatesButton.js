77cd5742b8a00594f454ccff330d98bf4c32c074
ewelinaszoda <ewelinaszoda@gmail.com>
2020-07-01 15:42:08 +0100

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+
+  createCoordinates  = (event) => {
+
+    const x = event.clientX
+    const y = event.clientY
+
+    this.props.onReceiveCoordinates([x,y])
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.createCoordinates}>Coordinates</button>
+      </div>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,39 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+  delayMethod = (event) => {
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // setTimeout, fetch, promises -> asynchronises 
+    // first the setTimeout need finish and after event can start 
+    // rare situation event pooling 
+
+      const eventDataINeed = {
+        clientX: event.clientX,
+        target: event.target,
+      }
+
+      setTimeout(() => {
+        this.props.onDelayedClick(eventDataINeed);
+      }, this.props.delay);
+
+    }
+
+  // another solution
+  //   event.persist();
+  //   window.setTimeout(() => {
+  //     this.props.onDelayedClick(event);
+  //   }, this.props.delay);
+  // };
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.delayMethod}>Delay</button>
+      </div>
+    )
+  }
+}
